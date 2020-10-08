254467426bb4e49e20158f429480e2e9338401a0
Rodrigo Marafelli <rodrigomarafelli@ioasys.com.br>
2020-10-07 22:10:47 -0300

    fixing request response

--- handler.js
+++ handler.js
@@ -4,25 +4,86 @@ const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-depe
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.extractMetadata = (event, context, callback) => {
-  console.log("entrou");
-  var params = {
-    TableName: "serverless-defiance-dev",
-    Item: {
-      s3objectkey: "1",
-      firstAttribute: "5",
-    },
+const s3 = new AWS.S3();
+
+module.exports.extractMetadata = (event) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " ")
+  );
+
+  const file_params = {
+    Bucket: bucket,
+    Key: key,
   };
 
-  dynamoDb.put(params, function (err, data) {
+  console.log("pathParameters " + event.pathParameters);
+
+  s3.getObject(file_params, function (err, data) {
     if (err) {
-      console.log("Error", err);
+      console.log(err);
     } else {
-      console.log("Success", data);
+      const object = {
+        TableName: "serverless-defiance-dev",
+        Item: {
+          s3objectkey: key,
+          fileLenght: data.ContentLength,
+        },
+      };
+
+      dynamoDb.put(object, function (err, data) {
+        if (err) {
+          console.log("Error", err);
+        } else {
+          console.log("Success", data);
+        }
+      });
     }
   });
-
-  console.log("passou da inserção no banco");
 };
 
-module.exports.getMetadata = (event, context, callback) => {};
+module.exports.getMetadata = (event, context, callback) => {
+  console.log("entrou");
+  // console.log("entrou" + s3objectkey);
+  // console.log("event" + event);
+  // console.log("context" + context);
+  // console.log("callback" + callback);
+  // console.log("request" + request);
+  // console.log("params" + params);
+  // try {
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({
+  //       message: "success",
+  //     }),
+  //   };
+  // } catch (err) {
+  //   console.log("error", err);
+  // }
+
+  var responseBody = {
+    key3: "value3",
+    key2: "value2",
+    key1: "value1",
+  };
+
+  var response = {
+    statusCode: 200,
+    headers: {
+      my_header: "my_value",
+    },
+    body: JSON.stringify(responseBody),
+    isBase64Encoded: false,
+  };
+  callback(null, response);
+};
+
+module.exports.hello = async (event, context, callback) => {
+  console.log("entrou");
+  return {
+    statusCode: 204,
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
