37fa67f7206535b2eff29c5da3b176cdb3dd5d27
Rodrigo Marafelli <rodrigomarafelli@ioasys.com.br>
2020-10-09 08:24:49 -0300

    improving syntax and addning gitignore file

--- handler.js
+++ handler.js
@@ -100,60 +100,81 @@ module.exports.listImages = async (event, context, callback) => {
   }
 };
 
-module.exports.biggestImage = async (event, context, callback) => {
+module.exports.infoImages = async (event, context, callback) => {
   const scanningParameters = {
     TableName: TableName,
   }
 
   const data = await getAllImages(scanningParameters)
 
+  let {smallest, biggest} = getSmallestAndBiggestImages(data)
+
+  let {jpgAmount, bmpAmount, gifAmount, pngAmount, existingImageFormats} = calculateFormatAmount(data)
+
+  let responseBody = {
+    smallestImage: smallest,
+    biggestImage: biggest,
+    jpgAmount: jpgAmount,
+    bmpAmount: bmpAmount,
+    gifAmount: gifAmount,
+    pngAmount: pngAmount,
+    allowedImageFormats: [".jpg", ".bmp", ".gif", ".png"],
+    existingImageFormats: existingImageFormats
+  }
+
+  if (data.Count > 0) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(responseBody)
+    }
+  }
+  else {
+    return {
+      statusCode: 204,
+    };
+  }
+};
+
+function calculateFormatAmount(data) {
+  let response = {
+    jpgAmount: 0,
+    bmpAmount: 0,
+    gifAmount: 0,
+    pngAmount: 0,
+    existingImageFormats: []
+  }
+
+  for (let i = 0; i < data.Count; i++) {
+    if (data.Items[i].s3objectkey.includes(".jpg")) response.jpgAmount ++
+    if (data.Items[i].s3objectkey.includes(".bmp")) response.bmpAmount ++
+    if (data.Items[i].s3objectkey.includes(".gif")) response.gifAmount ++
+    if (data.Items[i].s3objectkey.includes(".png")) response.pngAmount ++
+  }
+
+  if (response.jpgAmount > 0) response.existingImageFormats.push(".jpg")
+  if (response.bmpAmount > 0) response.existingImageFormats.push(".bmp")
+  if (response.gifAmount > 0) response.existingImageFormats.push(".gif")
+  if (response.pngAmount > 0) response.existingImageFormats.push(".png")
+
+  return response
+
+}
+
+function getSmallestAndBiggestImages(data) {
+  let smallest = data.Items[0]
   let biggest = data.Items[0]
   let i = 0
 
   for (i = 1; i < data.Count; i++) {
+    if (data.Items[i].fileLenght < smallest.fileLenght) smallest = data.Items[i];
     if (data.Items[i].fileLenght > biggest.fileLenght) biggest = data.Items[i];
   }
 
-  if (data.Count > 0) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(biggest)
-    }
+  return {
+    smallest: smallest,
+    biggest: biggest
   }
-  else {
-    return {
-      statusCode: 204,
-    };
-  }
-};
-
-module.exports.smallestImage = async (event, context, callback) => {
-  const scanningParameters = {
-    TableName: TableName,
-  }
-
-  const data = await getAllImages(scanningParameters)
-
-  let smallest = data.Items[0]
-  let i = 0
-
-  for (i = 1; i < data.Count; i++) {
-    console.log(data.Items[i].s3objectkey, data.Items[i].fileLenght)
-    if (data.Items[i].fileLenght < smallest.fileLenght) smallest = data.Items[i];
-  }
-
-  if (data.Count > 0) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(smallest)
-    }
-  }
-  else {
-    return {
-      statusCode: 204,
-    };
-  }
-};
+}
 
 function getAllImages(scanningParameters) {
   return new Promise((resolve) => {
@@ -193,7 +214,7 @@ module.exports.downloadImage = async (event, context, callback) => {
   } else {
     return {
       statusCode: 404,
-      body: JSON.stringify({message :"File not found"})
+      body: JSON.stringify({message :"Image not found"})
     };
   }
 
